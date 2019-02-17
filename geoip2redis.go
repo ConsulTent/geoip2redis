07bf1ccf488d869322673eb2df7c68fcfe3dc354
Alexey Zilber <AlexeyZilber@gmail.com>
2019-02-18 00:49:44 +0800

    final redis working

--- geoip2redis.go
+++ geoip2redis.go
@@ -2,31 +2,29 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-redis/redis"
 	"github.com/gocarina/gocsv"
+	"github.com/keimoon/gore"
 	"github.com/rburmorrison/go-argue"
 	"os"
 	"strconv"
 )
 
 type cmdline struct {
-	CsvFile        string `options:"required,positional" help:"CSV file with GeoIP data"`
-	Format         string `init:"f" options:"required" help:"ip2location|maxmind"`
-	RedisHost      string `init:"r" help:"Redis Host, default 127.0.0.1"`
-	RedisPort      int    `init:"p" help:"Redis Port, default 6379"`
-	RedisPass      string `init:"a" help:"Redis DB password, default none"`
-	InPrecision    int    `init:"i" options:"required" help:"Input precision.  This would be db file number.  See README.TXT"`
-	OutPrecision   int    `init:"o" help:"Output Precision.  Default: 0 (match input), see README.TXT"`
-	DontSkipHeader bool   `init:"s" help:"DON'T Skip the first CSV line. Default: skip, see README.TXT"`
+	CsvFile      string `options:"required,positional" help:"CSV file with GeoIP data"`
+	Format       string `init:"f" options:"required" help:"ip2location|maxmind"`
+	RedisHost    string `init:"r" help:"Redis Host, default 127.0.0.1"`
+	RedisPort    int    `init:"p" help:"Redis Port, default 6379"`
+	RedisPass    string `init:"a" help:"Redis DB password, default none"`
+	InPrecision  int    `init:"i" options:"required" help:"Input precision.  This would be db file number.  See README.TXT"`
+	OutPrecision int    `init:"o" help:"Output Precision.  Default: 0 (match input), see README.TXT"`
+	SkipHeader   bool   `init:"s" help:"Skip the first CSV line. Default: don't skip, see README.TXT"`
 }
 
-const pver = "0.0.1"
+const pver = "0.0.2"
 
 var gitver = "undefined"
 
 type csvstruct []struct{}
-
-var skiphdr bool
 
 var DEBUG = true
 
@@ -41,7 +39,7 @@ func main() {
 	var DBHDR string
 	var index int
 	var iprange int
-	var zcmd int64
+	//	var zcmd int64
 	//      var fakedata struct{}
 
 	fmt.Printf("GeoIP2Redis (c) 2019 ConsulTent Ltd. v%s-%s\n", pver, gitver)
@@ -77,10 +75,8 @@ func main() {
 		os.Exit(1)
 	}
 
-	if cmds.DontSkipHeader == true {
-		fmt.Println("WARNING: Not skipping header!")
-	} else {
-		skiphdr = true
+	if cmds.SkipHeader == true {
+		fmt.Println("WARNING: Skipping header!")
 	}
 
 	csvFile, err := os.OpenFile(cmds.CsvFile, os.O_RDWR|os.O_CREATE, os.ModePerm)
@@ -106,32 +102,30 @@ func main() {
 		fmt.Printf("RedisHost: %s, RedisPort: %d\n", cmds.RedisHost, cmds.RedisPort)
 	}
 
-	redisdb := redis.NewClient(&redis.Options{
-		Addr:     cmds.RedisHost + ":" + strconv.Itoa(cmds.RedisPort), // use default Addr
-		Password: cmds.RedisPass,                                      // no password set
-		DB:       0,                                                   // use default DB
-	})
-
-	_, err = redisdb.Ping().Result()
+	redisdb, err := gore.Dial(cmds.RedisHost + ":" + strconv.Itoa(cmds.RedisPort))
 	if err != nil {
-		//	panic(err)
+		panic(err)
 	}
+
+	defer redisdb.Close()
 
 	i = 0 // i is the outer line range
 	for _, sample := range samples {
 		x = 0 // x is the inner column range
 		for _, cell := range sample {
-			if !(i == 0 && skiphdr == true) {
+			if !(i == 0 && cmds.SkipHeader == true) {
 				if !(skipcol == x) {
 					//push unto string array
 					index = len(rediscmd)
 					if x == 0 {
-						rediscmd = rediscmd[:index] + cell + " \"" + cell
+						iprange, _ = strconv.Atoi(cell)
+						//		rediscmd = rediscmd[:index] + cell + " \"" + cell
+						rediscmd = rediscmd[:index] + cell
 					} else {
 						rediscmd = rediscmd[:index] + "|" + cell
 					}
 				} //skipcol
-			} //skiphdr
+			} //SkipHeader
 			x++
 		} // cell range
 
@@ -140,12 +134,16 @@ func main() {
 			fmt.Printf("Skipped Header\n")
 		} else {
 			index = len(rediscmd)
-			rediscmd = rediscmd[:index] + "\""
+			//		rediscmd = rediscmd[:index] + "\""
 			// if DEBUG == true { fmt.Printf("REDIS<: %s\nx: %d, i: %d\n", rediscmd, x, i) }
-			zcmd, err = redisdb.ZAdd(DBHDR, iprange, rediscmd).Result()
+			_, err = gore.NewCommand("ZADD", DBHDR, iprange, rediscmd).Run(redisdb)
+			if err != nil {
+				panic(err)
+			}
 			rediscmd = ""
 		}
 		i++
 	}
 
+	fmt.Printf("Loaded %d entries\n", i)
 }
--- go.mod
+++ go.mod
@@ -3,5 +3,6 @@ module consultent/go-geoip2redis
 require (
 	github.com/go-redis/redis v6.15.1+incompatible
 	github.com/gocarina/gocsv v0.0.0-20190129143436-91f6fc13d488
+	github.com/keimoon/gore v0.0.0-20160317032603-1ac8b93b5fdb
 	github.com/rburmorrison/go-argue v0.0.0-20190107184430-e164876050e3
 )
