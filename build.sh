b901a9c54258cc2a4b3e56cc6a1d0ff44407f80d
Alexey Zilber <AlexeyZilber@gmail.com>
2020-01-20 19:12:44 +0800

    added maxmind tool to build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 export GOPATH=$HOME/go
 go build -i -v -ldflags="-X main.gitver=$(git describe --always --long --dirty)" -o geoip2redis *.go
 go build -i -v -ldflags="-X main.gitver=$(git describe --always --long)" -o tools/ip2long/ip2long tools/ip2long/ip2long.go
+go build -i -v -ldflags="-X main.gitver=$(git describe --always --long)" -o tools/maxmind-ip2location/maxmind-ip2location tools/maxmind-ip2location/maxmind-ip2location.go
